045a17e336ad149923fc7d3e0576acd3d8c25358
Steven Haryanto (on PC, Bandung) <stevenharyanto@gmail.com>
2014-06-24 15:33:04 +0700

    Initial commit

---
+++ lib/App/ProgUtils.pm
@@ -0,0 +1,25 @@
+package App::ProgUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# VERSION
+# DATE
+
+1;
+# ABSTRACT: Command line to manipulate programs in PATH
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+ progless
+ progedit
+ progman
+
+=head1 SEE ALSO
+
+B<which>
+
+=cut
