144653ea292745123cb36f2230f67f1ed452df04
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-06-27 22:21:03 +0700

    Refactor: remove duplicated code

--- lib/App/ProgUtils.pm
+++ lib/App/ProgUtils.pm
@@ -7,6 +7,17 @@ use warnings;
 # VERSION
 # DATE
 
+our $_complete_program = sub {
+    require Complete::Util;
+    my %args = @_;
+    Complete::Util::mimic_shell_dir_completion(
+        completion => Complete::Util::complete_program(
+            word      => $args{word},
+            ci        => 1,
+        )
+    );
+};
+
 1;
 # ABSTRACT: Command line to manipulate programs in PATH
 
