b2662f3d3ab3cf0e9b176f7841803b979efd802d
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-11 17:03:29 +0700

    Also complete file is word contains path separator /

--- lib/App/ProgUtils.pm
+++ lib/App/ProgUtils.pm
@@ -11,12 +11,28 @@ our $_complete_program = sub {
     require Complete::Util;
     my %args = @_;
 
-    {
-        completion => Complete::Util::complete_program(
+    my $word = $args{word} // '';
+    my $completion;
+    my $is_path;
+    if ($word =~ m#/#) {
+        # if user specify path, e.g. ./foo, ../bar, or /usr/bin/ it means she
+        # wants to complete from filesystem. but note that complete_file()
+        # doesn't yet support ci=>1 option.
+        $is_path = 1;
+        $completion = Complete::Util::complete_file(
+            word => $word,
+            #ci   => 1, # convenience
+        );
+    } else {
+        $completion = Complete::Util::complete_program(
             word      => $args{word},
             ci        => 1, # convenience
-        ),
-        is_path    => 1,
+        );
+    }
+
+    {
+        completion => $completion,
+        is_path    => $is_path,
     };
 };
 
